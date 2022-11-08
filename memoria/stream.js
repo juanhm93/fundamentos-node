2428c4485939043d717d4a288592f7532979e14b
Juan M Hernandez <juanhm93@gmail.com>
2022-11-07 21:17:39 -0500

    work stream in filesystem to buffer

---
+++ memoria/stream.js
@@ -0,0 +1,41 @@
+
+const fs = require('fs');
+const stream = require('stream')
+const util = require('util')
+
+let data = ''
+
+let readableStream = fs.createReadStream(__dirname + '/input.txt')
+readableStream.setEncoding('UTF8');
+
+
+// readableStream.on('data', function(chunk) {
+//  data += chunk;
+//  // console.log(chunk)
+// })
+
+// readableStream.on('end', function(){
+//  console.log(data)
+// })
+
+// process.stdout.write('hola')
+// process.stdout.write('que')
+// process.stdout.write('tal')
+
+const Transform = stream.Transform;
+
+function Mayus(){
+ Transform.call(this)
+}
+
+util.inherits(Mayus, Transform);
+
+Mayus.prototype._transform = function(chunk, codif, callback){
+ chunkMayus = chunk.toString().toUpperCase();
+ this.push(chunkMayus)
+ callback()
+}
+
+let mayus = new Mayus();
+
+readableStream.pipe(mayus).pipe(process.stdout)
